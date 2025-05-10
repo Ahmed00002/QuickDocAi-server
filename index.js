671f8e86fc1c891f108ef6e4613faf6bd2c3b6fb
Ahmed Numan <reduanahmed773@gmail.com>
2025-05-10 17:25:30 +0600

    jwt verification added

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 import e from "express";
 import cors from "cors";
+import jwt from "jsonwebtoken";
+import "dotenv/config";
 
 const app = e();
 const port = 3000;
-app.use(cors());
+app.use(cors(["http://localhost:5173"]));
 app.use(e.json());
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
+const verifyToken = (req, res, next) => {
+  //   console.log(req);
+  const authorization = req.headers["authorization"];
+  const token = authorization && authorization.split(" ")[1];
+  if (!token) {
+    return res.send("A token is required for authentication");
+  }
+
+  jwt.verify(token, process.env.JWT_PRIVATE_KEY, (err, user) => {
+    if (err) {
+      return res.status(403).send("Invalid Token");
+    }
+    req.user = user;
+  });
+  next();
+};
+
+app.get("/verifyJWT", verifyToken, (req, res) => {
+  res.send("Verification successful");
 });
 
 app.listen(port, () => {
